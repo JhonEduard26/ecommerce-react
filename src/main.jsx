c0eebfdc6839b22f2392fa3c90c185e5675e9ccb
jhon <jhon.bocanegra26@gmail.com>
2023-05-18 12:50:33 -0500

    agregado contexto global

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import { RouterProvider } from 'react-router-dom'
 
 import router from './router/root.jsx'
 import './index.css'
+import { ShoppingCartProvider } from './context/ShoppingCartContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ShoppingCartProvider>
+      <RouterProvider router={router} />
+    </ShoppingCartProvider>
   </React.StrictMode>,
 )
